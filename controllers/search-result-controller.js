f303828e9cbf73021ef957e349dd1d81999899f0
unknown <you@example.com>
2023-01-07 23:51:16 +0500

    Finalize the search result route

--- controllers/search-result-controller.js
+++ controllers/search-result-controller.js
@@ -2,12 +2,48 @@ const pool = require('../db.config')
 
 
 const getDoctorData = async (req, res) => {
-    const searchParameter = req.query
+    const {userInput} = req.query
+    console.log(userInput)
     const searchQuery = `SELECT
 	doctor.first_name,
     doctor.last_name,
     qualification.education,
-    qualification.sepecialization,
+    qualification.specialization,
+    doctor.doctor_image,
+    doctor.city,
+    review.rating,
+    appointment.payment,
+    location.location_name
+FROM
+	doctor
+INNER JOIN qualification ON qualification.doctor_id = doctor.doctor_id
+INNER JOIN review ON review.doctor_id = doctor.doctor_id
+INNER JOIN location ON location.doctor_id = doctor.doctor_id
+INNER JOIN appointment ON appointment.doctor_id = doctor.doctor_id
+WHERE
+	 doctor.first_name ILIKE $1 OR qualification.specialization ILIKE $1`
+
+    try {
+        const result = await pool.query(searchQuery, [userInput.concat("%")])
+        // res.status(200).json(result)
+        if (result.rowCount > 0) {
+            res.status(200).json(result.rows)
+        }
+        else {
+            res.status(404).json({ message: `No Search Result Found..` })
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+const filterDoctorData = async () => {
+    const searchParameter = req.body
+    const searchQuery = `SELECT
+	doctor.first_name,
+    doctor.last_name,
+    qualification.education,
+    qualification.specialization,
     doctor.image,
     doctor.city,
     review.rating,
@@ -22,22 +58,22 @@ INNER JOIN appointment ON appointment.doctor_id = doctor.doctor_id
 WHERE
 	qualification.specialization = $1 OR doctor.first_name = $1;
     ORDER BY $1`
-}
-try{
-    const result = await pool.query(searchQuery,[searchParameter])
-    if(result.rows > 0){
-        res.status(200).json(result.rows)
+
+    try {
+        const result = await pool.query(searchQuery, [searchParameter])
+        if (result.rows > 0) {
+            res.status(200).json(result.rows)
+        }
+        else {
+            res.status(404).json({ message: `No Search Result Found..` })
+        }
+    } catch (error) {
+        throw error
     }
-    else{
-        res.status(404).json({message:`No Search Result Found..`})
-    }
-}catch(error){
-    throw error
 }
 
 
 
 
 
-
-module.exports = {getDoctorData}
+module.exports = { getDoctorData }
--- routes/search-result.js
+++ routes/search-result.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 
 
-router.get("/search",getDoctorData )
+router.get("/api/search",getDoctorData )
 
 
 module.exports = router;
