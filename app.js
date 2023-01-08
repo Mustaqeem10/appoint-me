422cb836561396f8df2de89e3251b7aa2971fc9a
unknown <usamamehar439@gmail.com>
2023-01-09 00:42:00 +0500

    Delete doctor middleware from app.js

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const app = express()
 require('dotenv').config()
 app.use(express.json())
 
-// const doctorRoutes = require('./routes/doctor')
-// app.use('/doctor', doctorRoutes)
 
 const homeRoutes = require('./routes/home')
 app.use('/', homeRoutes)
 
 const PORT = process.env.DEV_PORT
-
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`)
 })
