20a125c565c383e266878a8a14c5e525ef8cc3a7
unknown <usamamehar439@gmail.com>
2023-01-09 00:38:48 +0500

    Update Code

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const app = express()
-
 require('dotenv').config()
-
 app.use(express.json())
 
-const doctorRoutes = require('./routes/doctor')
-app.use('/doctor', doctorRoutes)
+// const doctorRoutes = require('./routes/doctor')
+// app.use('/doctor', doctorRoutes)
 
 const homeRoutes = require('./routes/home')
 app.use('/', homeRoutes)
